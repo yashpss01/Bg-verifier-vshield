2913dc58a4e9eb6fc3f83fa9aa85d9dcd9731213
Yash Pratap Singh Solanki <yashpratap0143@gmail.com>
2026-05-21 10:36:35 +0530

    style: prevent icon overlap by using Tailwind important flag on input left paddings

--- frontend/src/pages/CandidateList.tsx
+++ frontend/src/pages/CandidateList.tsx
@@ -196,7 +196,7 @@ const CandidateList: React.FC = () => {
             <input
               type="text"
               placeholder="Search by name, email, phone..."
-              className="block w-full h-9 glass-input pl-10 text-xs"
+              className="block w-full h-9 glass-input !pl-10 text-xs"
               value={search}
               onChange={(e) => setSearch(e.target.value)}
             />
